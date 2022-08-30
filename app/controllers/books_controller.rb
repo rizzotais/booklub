af259f53747d3ebe6570a51100436431b7f512e8
marckanderson <2mandersonb@gmail.com>
2022-08-30 17:52:30 -0300

    frontshow

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,8 +4,8 @@ before_action :set_book, only: %i[new create]
   def index
     @books = Book.all
   end
-  
+
   def show
-    @booking = Book.find(params[:id])
+    @book = Book.find(params[:id])
   end
 end
