848b93d050996bbffc82ff24dd6798426a12166f
marckanderson <2mandersonb@gmail.com>
2022-08-31 15:34:26 -0300

    finishedshow

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,10 @@ class Book < ApplicationRecord
   has_many :reviews
   has_many :users, through: :readings
   has_many :users, through: :reviews
+  include PgSearch::Model
+  pg_search_scope :search_by_title_and_author,
+    against: [ :title, :author ],
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
 end
