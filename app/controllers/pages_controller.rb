4d2a3fbede15b4a60cf426c25616f1671790a80a
Juan Bezanilla <juanbezanillaz@gmail.com>
2022-09-01 16:55:50 -0400

    links

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :contactus ]
 
   def home
+  end
+
+
+  def contactus
   end
 end
