8b8dc51adba42df044459c5f183a44fb8acedc65
Tais Rizzo <rizzotais@gmail.com>
2022-09-01 12:12:23 -0400

    message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def display_img(book)
     if book.img_book == nil
-      asset_url("books.png")
+      asset_url("https://drive.google.com/file/d/16aAVCbfA6lRPVytm92UL7MPlfpOZUxJk/view?usp=sharing")
     else
       book.img_book
     end
