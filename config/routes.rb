8cf81aefed56e63a2a7a493c494b2a51655ba2f3
marckanderson <2mandersonb@gmail.com>
2022-09-05 19:02:00 -0300

    notready

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,12 @@ Rails.application.routes.draw do
     resources :bookmarks, only: %i[create]
     resources :reviews, only: %i[new create]
   end
-
   resources :users, only: [:show]
   resources :lists, except: %i[edit update]
+
+  resources :chatrooms, only: :show
+  resources :chatrooms, only: :show do
+    resources :messages, only: :create
+  end
+
 end
