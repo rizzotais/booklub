773ff91faa1316333632617c30367de3c611b740
Tais Rizzo <rizzotais@gmail.com>
2022-09-05 13:48:28 -0400

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @book = Book.where("title LIKE ?", "%Harry Potter").first
+    @reading = Reading.new
   end
 end
